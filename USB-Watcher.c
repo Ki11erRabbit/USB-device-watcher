27123fe6cad2dfaa09e0bbe01f7283c5a1e9484b
ki11errabbit <ki11errabbit@posteo.net>
2022-08-12 15:37:25 -0700

    reads config file at ~/.config/USB-watcher.conf

--- USB-Watcher.c
+++ USB-Watcher.c
@@ -11,6 +11,7 @@ typedef struct CmdSet {
 	char *onPlugIn;
 	char *onUnplug;
 	char activated;
+	char found;
 } CmdSet;
 
 
@@ -37,6 +38,7 @@ CmdSet* setup(char* vendorId,char* productId, char* onPlugIn, char* onUnplug) {
 	temp->onPlugIn = strdup(onPlugIn);
 	temp->onUnplug = strdup(onUnplug);
 	temp->activated = 0;
+	temp->found = 0;
 
 	return temp;
 }
@@ -48,14 +50,77 @@ int main(void)
     struct dirent *de; // Pointer for directory entry
     struct dirent *nde; // Pointer for directory entry
     // opendir() returns a pointer of DIR type.
-
+    
+    char * homeDir = getenv("HOME");
+    //printf("%s\n", homeDir);
     char initialDirectory[21] = "/sys/bus/usb/devices/";
     char buffer[50];
     char found = 0;
     char activated = 0;
 
+    CmdSet* devices[50];
+    size_t totalDevices = 0;
     CmdSet* model_m;
-    model_m = setup("17f6", "0822", "model-m-helper", "setxkbmap -layout us"); 
+    //devices[0] = setup("17f6", "0822", "model-m-helper", "setxkbmap -layout us"); 
+
+    cat(buffer,homeDir, "/.config",0);
+    DIR *homeDr = opendir(buffer);
+
+    if (homeDr == NULL) {
+	    printf("Could not open user home's .config\n");
+	    return 1;
+    }
+
+    while ((de = readdir(homeDr)) != NULL) {
+	    if (strstr(de->d_name, "USB-watcher.conf")) {
+		    char tempBuffer[50];
+		    cat (tempBuffer, buffer, "USB-watcher.conf",1);
+		    printf ("%s\n", tempBuffer);
+
+		    FILE *fptr = fopen(tempBuffer, "r");
+		    if (fptr == NULL) {
+			    printf("Unable to open file\n");
+			    return 1;
+		    }
+		    
+		    //TODO: put in logic for reading lines
+		    char *lineBuffer = NULL;
+		    size_t lineBufferSize = 50;
+		    char lineCount = 0;
+		    char *vendorId = NULL, *productId = NULL,*plugIn = NULL,*unplug = NULL;
+		    while (getline(&lineBuffer,&lineBufferSize,fptr)) {
+			    if (lineBuffer[0] == '\0') break;
+			    if (lineBuffer[0] == '\n') goto skip;
+			    printf("%s", lineBuffer);
+			    lineCount++;
+			    if (lineCount -1 == 0 ) {
+				vendorId = strtok(lineBuffer,"/");
+			    	productId = strtok(NULL,"\n");
+				printf("%s/%s\n", vendorId, productId);
+			    }
+			    else if (lineCount -1 == 1) plugIn = strtok(lineBuffer,"\n");
+			    else if (lineCount -1 == 2) {
+				unplug = strtok(lineBuffer,"\n");
+				lineCount = 0;
+				devices[totalDevices] = setup(vendorId, productId, plugIn,unplug);
+				totalDevices++;
+				vendorId = NULL;
+				productId = NULL;
+				plugIn = NULL;
+				unplug = NULL;
+			    }
+
+
+
+
+
+			    skip:
+			    lineBuffer = NULL;
+		    }
+		    fclose(fptr);
+	    }
+    }
+	
 
     // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html
     // for readdir()
@@ -65,7 +130,7 @@ int main(void)
         if (dr == NULL) // opendir returns NULL if couldn't open directory
         {
             printf("Could not open current directory" );
-            return 0;
+            continue;
         }
 
         while ((de = readdir(dr)) != NULL) {
@@ -117,21 +182,53 @@ int main(void)
                     }
 
                 }
-                if (strstr(idVendor, model_m->idVendor) && strstr(idProduct, model_m->idProduct)) {
-                    //printf("\t%s/%s\n", idVendor, idProduct);
-                    found = 1;
-                    for (int i = 0; i < 8; i++) {
-                        idVendor[i] = '\0';
-                        idProduct[i] = '\0';
-                    }
-                    break;
-                }
+		for (size_t i = 0; i < totalDevices; i++) {
+					
+                	if (strstr(idVendor, devices[i]->idVendor) && strstr(idProduct, devices[i]->idProduct)) {
+                    		//printf("\t%s/%s\n", idVendor, idProduct);
+                    		devices[i]->found = 1;
+                    		for (int i = 0; i < 8; i++) {
+                        		idVendor[i] = '\0';
+                        		idProduct[i] = '\0';
+                    		}
+                	}
+
+
+		}
+
 
                 closedir(ndr);
                 //printf("%s\n", de->d_name);
             }
         }
-        if (found == 1) {
+	for (size_t i = 0; i < totalDevices; i++) {
+		if (devices[i]->found == 1) {
+			if (devices[i]->activated == 0) {
+				printf("USB devices attached\n");
+				int result = system(devices[i]->onPlugIn);
+				printf("%d",result);
+				devices[i]->activated = 1;
+			}
+			else {
+				printf("USB device still plugged in\n");
+			}
+				sleep(1);
+		}
+		else {
+			if (devices[i]->activated == 1) {
+				printf("USB device detached\n");
+				system(devices[i]->onUnplug);
+				devices[i]->activated = 0;
+			}
+			else {
+				printf("USB device unplugged\n");
+			}
+			sleep(1);
+		}
+		devices[i]->found = 0;
+
+	}
+       /* if (found == 1) {
             printf("USB device attached\n");
             if (model_m->activated == 0) {
                 sleep(1);
@@ -148,7 +245,7 @@ int main(void)
             system(model_m->onUnplug);
             model_m->activated = 0;
         }
-        found = 0;
+        found = 0;*/
         closedir(dr);
     }
     return 0;
