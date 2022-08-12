5b34bfea105356cdadfb37f30ad04f4e4d1ecd56
ki11errabbit <ki11errabbit@posteo.net>
2022-08-12 10:36:59 -0700

    added cmake, change code to use a struct

---
+++ build/CMakeFiles/USB-Watcher.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/USB-Watcher.dir/USB-Watcher.c.o"
+  "CMakeFiles/USB-Watcher.dir/USB-Watcher.c.o.d"
+  "USB-Watcher"
+  "USB-Watcher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/USB-Watcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
